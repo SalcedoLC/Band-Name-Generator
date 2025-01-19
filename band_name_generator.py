3e12e0dba15597a4817f0c31b955ed1533b7e6f5
Pedro Salcedo <psalcedo@mail.valenciacollege.edu>
2025-01-19 16:43:47 -0500

    band_name_generator.py

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,4 +1,4 @@
-print("Welcome to the Band Name Generator")
+print("Welcome to the Band Name Generator\n")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pets name?\n")
 print("Your Band name could be: " + city + " " + pet)
