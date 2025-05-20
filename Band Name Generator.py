20d47bff7624e00c1bb5a21a9537610b89ad2943
Pedro Salcedo <psalcedo@mail.valenciacollege.edu>
2025-05-20 15:31:42 -0400

    Add Band Name Generator script
    Initial upload of my band name generator project. Simple Python script that asks for user input and prints a custom band name. Part of my Python learning journey.

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the 'Band Name Generator'\n")
+
+city = input("What's the name of the city you grew up in?\n").title()
+pet_name = input("What's your pet's name?\n").title()
+print("Your band name could be: " + city + " " + pet_name)
